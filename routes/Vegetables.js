3daf41b2ac448096223c538255ea3164b6f79346
Karthik Parvathaneni <123134732+Karthik202012@users.noreply.github.com>
2023-04-20 19:34:05 -0500

    updated

--- routes/Vegetables.js
+++ routes/Vegetables.js
@@ -10,22 +10,22 @@ var router = express.Router();
 //  res.redirect("/login");
 //  }
 
-/* GET flowers */
+/* GET vegetables */
 router.get("/", vegetables_controller.vegetables_view_all_Page);
 
-// GET request for one flower.
+// GET request for one vegetables.
 router.get("/Vegetables/:id", vegetables_controller.vegetables_detail);
 
-/* GET detail costume page */
+/* GET detail vegetables page */
 router.get("/detail", vegetables_controller.vegetables_view_one_Page);
 
-/* GET create costume page */
+/* GET create vegetables page */
 router.get("/create", vegetables_controller.vegetables_create_Page);
 
 /* GET create update page */
 router.get("/update", vegetables_controller.vegetables_update_Page);
 
-/* GET delete costume page */
+/* GET delete vegetables page */
 router.get("/delete", vegetables_controller.vegetables_delete_Page);
 
 module.exports = router;
